f2cebc2ae387992001855c93a111e9643b549d44
Olusola Disu <soladisu@gmail.com>
2023-03-16 21:11:37 -0700

    Size, Grandeur, and territory

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,16 +7,10 @@
  */
 int main(void)
 {
-	char v;
-	int w;
-	long int x;
-	long long int y;
-	float z;
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(v));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(w));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(x));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(y));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(z));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
