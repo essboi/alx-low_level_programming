7ce89c9bf02e269ac7f568111024cc369248be6f
Olusola Disu <soladisu@gmail.com>
2023-03-16 20:47:22 -0700

    Size, grandeur, and territory

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char v;
+	int w;
+	long int x;
+	long long int y;
+	float z;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(v));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(w));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(x));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(y));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(z));
+	return (0);
+}
