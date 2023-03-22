2ef16fb7b178d8d4e65845d5dda47e2f7f298cb7
Olusola Disu <soladisu@gmail.com>
2023-03-22 15:22:53 -0700

    Fibonacci Numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints first 50 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int count;
+	unsigned long fib_1 = 0, fib_2 = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib_1 + fib_2;
+		printf("%lu", sum);
+
+		fib_1 = fib_2;
+		fib_2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(",");
+	}
+
+	return (0);
+}
