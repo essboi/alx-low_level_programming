1cbff725a22e86c4bc6154599cf458bc0b6089ef
Olusola Disu <soladisu@gmail.com>
2023-03-22 15:39:01 -0700

    Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - This program finds and prints the sum of the even-valued terms
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	float total_sum;
+	unsigned long fib_1 = 0, fib_2 = 1, fib_sum;
+
+	while (1)
+	{
+		fib_sum = fib_1 + fib_2;
+
+		if (fib_sum > 4000000)
+			break;
+
+		if ((fib_sum % 2) == 0)
+			total_sum += fib_sum;
+
+		fib_1 = fib_2;
+		fib_2 = fib_sum;
+	}
+	printf("%0f\n", total_sum);
+
+	return (0);
+}
