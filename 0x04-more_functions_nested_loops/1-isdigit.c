abf8853b9d39c7217dcf3fc0157d4d3868a6de45
Olusola Disu <soladisu@gmail.com>
2023-03-23 16:48:24 -0700

    DIgit checker

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - This function checks for digits 0 through 9
+ *
+ * @c: Parameter to be checked
+ *
+ * Return: 0 or 1
+ *
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
