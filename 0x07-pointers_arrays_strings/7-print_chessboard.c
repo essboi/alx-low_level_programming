e9488ea79e34406598e9be5b8cdaa03227b616ac
Olusola Disu <soladisu@gmail.com>
2023-04-03 20:17:59 -0700

    Chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,15 +10,12 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	int x, y;
 
-	for (i = 0; i < 8; i++)
+	for (x = 0; x < 8; x++)
 	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(a[i][j]);
-			_putchar(' ');
-		}
+		for (y = 0; y < 8; y++)
+			_putchar(a[x][y]);
 		_putchar('\n');
 	}
 }
