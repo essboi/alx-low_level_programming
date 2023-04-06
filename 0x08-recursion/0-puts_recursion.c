838e4b983e08e1f9dd64d223c756911a6f7adb9f
Olusola Disu <soladisu@gmail.com>
2023-04-05 18:01:31 -0700

    Print a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _puts_recursion - This function prints a string
+  * followed by a new line
+  *
+  * @s: String parameter
+  *
+  * Return: void
+  */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
