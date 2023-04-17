d11b246581f4aebcb40096790535d1aa297d10f7
Olusola Disu <soladisu@gmail.com>
2023-04-17 12:33:15 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - Dog class
+ *
+ * @name: Dog's name
+ *
+ * @age: Dog's age
+ *
+ * @owner: Dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
