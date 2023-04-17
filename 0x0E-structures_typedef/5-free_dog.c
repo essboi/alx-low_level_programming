b5bef87433a17c054e7203ef132846262140b90f
Olusola Disu <soladisu@gmail.com>
2023-04-17 13:04:39 -0700

    Free the dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+  * free_dog - This funtion frees dog
+  *
+  * @d: Pointer to the dog to be freed
+  *
+  * Return: void
+  */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
