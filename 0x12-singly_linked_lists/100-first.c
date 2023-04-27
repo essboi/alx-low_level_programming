b2a86aec04f90a1a9cb7e91a0ceb7644bf1195cc
Olusola Disu <soladisu@gmail.com>
2023-04-27 10:24:57 -0700

    1st commit-HareandTortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * theHareandTortoise - Apply the constructor attribute to myStartupFun()
+ * so that it is executed before main()
+ */
+
+void theHareandTortoise(void) __attribute__ ((constructor));
+
+/**
+ * theHareandTortoise - implementation of theHareandTortoise
+ */
+
+void theHareandTortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
